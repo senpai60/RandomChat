2719ccabd6881997b5307438ece3a2c8b5bf09e0
senpai60 <sathloniyavivek64@gmail.com>
2026-02-10 20:06:25 +0530

    sever initialised

---
+++ server/server.js
@@ -0,0 +1,6 @@
+import server from "./src/app.js";
+const port = 3000;
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+import { createServer } from "http";
+
+import cors from "cors";
+import morgan from "morgan";
+import { Server } from "socket.io";
+
+app.use(cors());
+app.use(morgan("dev"));
+
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+export default server;
