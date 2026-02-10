38ef95054ca72f8302c89be2dea0c4364864a9aa
senpai60 <sathloniyavivek64@gmail.com>
2026-02-10 22:48:53 +0530

    feat: initialize server entry point and ignore server build directories.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 import server from "./src/app.js";
-const port = 3000;
 
-server.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+server.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
 });
